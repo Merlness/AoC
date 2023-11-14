705d80692bceb855ed11739c11206e6db19bfde8
Merl <merlfmartin@gmail.com>
2023-11-14 10:30:32 -0700

    day3 2015 oop

--- 2015/day_3_oo.py
+++ 2015/day_3_oo.py
@@ -1,20 +1,40 @@
 class Santa:
+    # try to have your getters do little work - readonly
+    # while setters do all the work - write
+    # put for loop of calc wrap and ribbon in wrap
+
     def __init__(self):
         self.houses = 1
         self.x = 0
         self.y = 0
+        self.a = 0
+        self.b = 0
         self.coordinates = {(0, 0)}
+        self.robo_coordinates = {(0, 0)}
+        self.houses = 0
+        self.robo_santa_houses = 0
+        self.santas_turn = True
+        
     
-    def get_houses(self, moves):
+    def get_houses(self):
+        return self.houses
+    
+    def get_robo_and_santa_houses(self):
+        return self.robo_santa_houses
+
+    def recieve_message(self, message):
+        self.houses = self.decode_message(message)
+        self.robo_santa_houses = self.decode_robo_and_santa(message)
+
+    def decode_message(self, moves):
         X = self.x
         Y = self.y
 
         for move in moves:
             X, Y = self.track_santa(move, X, Y)
             self.coordinates.add((X, Y))
-            houses = len(self.coordinates)
 
-        return houses
+        return len(self.coordinates)
 
     def track_santa(self, move, x, y):
         if move == '>':
@@ -27,4 +47,26 @@ class Santa:
             y -= 1
 
         return x, y
+    
+    def decode_robo_and_santa(self, moves):
+        X = self.x
+        Y = self.y
+        A = self.a
+        B = self.b
+
+        for move in moves:
+            if self.santas_turn:
+                X, Y = self.track_santa(move, X, Y)
+                self.robo_coordinates.add((X, Y))
+            else:
+                A, B = self.track_santa(move, A, B)
+                self.robo_coordinates.add((A, B))
+
+            self.santas_turn = not self.santas_turn
+        
+        return len(self.robo_coordinates)
+
+
+
+    
         
--- 2015/test_day_3_oo.py
+++ 2015/test_day_3_oo.py
@@ -17,57 +17,72 @@ class Test_Day_3_Part_1(unittest.TestCase):
     def test_no_move(self):
         direction = ['']
         santa = sut.Santa()
-        houses = santa.get_houses(direction) 
+        santa.recieve_message(direction)
+        houses = santa.get_houses() 
         self.assertEqual(houses, 1)
 
     def test_1_move(self):
         direction = ['>']
         santa = sut.Santa()
-        houses = santa.get_houses(direction) 
+        santa.recieve_message(direction)
+        houses = santa.get_houses() 
         self.assertEqual(houses, 2)
     
     def test_2_moves(self):
         direction = ['^', 'v']
         santa = sut.Santa()
-        houses = santa.get_houses(direction)  
+        santa.recieve_message(direction)
+        houses = santa.get_houses()   
         self.assertEqual(houses, 2)
 
     def test_4_moves(self):
         direction = ['^', '>', 'v', '<']
         santa = sut.Santa()
-        houses = santa.get_houses(direction)
+        santa.recieve_message(direction)
+        houses = santa.get_houses() 
         self.assertEqual(houses, 4)
 
     def test_all_moves(self):
         santa = sut.Santa()
-        houses = santa.get_houses(santa_directions) 
+        santa.recieve_message(santa_directions)
+        houses = santa.get_houses()  
         self.assertEqual(houses, 2592)
 
-# class Test_Day_3_Part_2(unittest.TestCase):
+class Test_Day_3_Part_2(unittest.TestCase):
 
-#     def test_no_move(self):
-#         direction = ['']
-#         houses = sut.part_2(direction) 
-#         self.assertEqual(houses, 1)
+    def test_no_move(self):
+        direction = ['']
+        santa = sut.Santa()
+        santa.recieve_message(direction)
+        houses = santa.get_robo_and_santa_houses() 
+        self.assertEqual(houses, 1)
 
-#     def test_1_move_each(self):
-#         direction = ['^','v']
-#         houses = sut.part_2(direction) 
-#         self.assertEqual(houses, 3)
+    def test_1_move_each(self):
+        direction = ['^','v']
+        santa = sut.Santa()
+        santa.recieve_message(direction)
+        houses = santa.get_robo_and_santa_houses()
+        self.assertEqual(houses, 3)
 
-#     def test_3_moves_each(self):
-#         direction = ['^', '>','v', '<']
-#         houses = sut.part_2(direction) 
-#         self.assertEqual(houses, 3)
+    def test_3_moves_each(self):
+        direction = ['^', '>','v', '<']
+        santa = sut.Santa()
+        santa.recieve_message(direction)
+        houses = santa.get_robo_and_santa_houses() 
+        self.assertEqual(houses, 3)
 
-#     def test_5_moves_each(self):
-#         direction = ['^','v', '^', 'v', '^', 'v', '^', 'v', '^', 'v']
-#         houses = sut.part_2(direction) 
-#         self.assertEqual(houses, 11)
+    def test_5_moves_each(self):
+        direction = ['^','v', '^', 'v', '^', 'v', '^', 'v', '^', 'v']
+        santa = sut.Santa()
+        santa.recieve_message(direction)
+        houses = santa.get_robo_and_santa_houses() 
+        self.assertEqual(houses, 11)
     
-#     def test_all_moves(self):
-#         houses = sut.part_2(santa_directions) 
-#         self.assertEqual(houses, 2360)
+    def test_all_moves(self):
+        santa = sut.Santa()
+        santa.recieve_message(santa_directions)
+        houses = santa.get_robo_and_santa_houses() 
+        self.assertEqual(houses, 2360)
 
 
 if __name__ == '__main__':
